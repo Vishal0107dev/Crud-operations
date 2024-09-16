70da9f30f3e49e6b670280db32d31e488c6eff61
Vishal0107dev <vishaldevops55@gmail.com>
2024-09-16 11:47:09 +0530

    CRUD

---
+++ src/EmployeeData.js
@@ -0,0 +1,20 @@
+export const EmployeeData = [
+    {
+        id : 1,
+        firstName : 'Vishal',
+        lastName : 'Lamani',
+        age : 20
+    },
+    {
+        id : 2,
+        firstName : 'Samarth',
+        lastName : 'Bhosle',
+        age : 20
+    },
+    {
+        id : 3,
+        firstName : 'Awinash',
+        lastName : 'Naruto',
+        age : 19
+    },
+]
